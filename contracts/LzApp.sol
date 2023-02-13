5881d6f2d92b55810b0239b5e1cb0943b3c47c14
LevX <team@levx.io>
2023-02-13 22:08:28 +0900

    Initial commit

---
+++ contracts/LzApp.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/ILayerZeroReceiver.sol";
+import "./interfaces/ILayerZeroUserApplicationConfig.sol";
+import "./interfaces/ILayerZeroEndpoint.sol";
+import "./libraries/BytesLib.sol";
+
+error Forbidden();
+error NoTrustedRemote();
+error MinGasLimitNotSet();
+error GasLimitTooLow();
+error InvalidAdapterParams();
+error NoTrustedPathRecord();
+error InvalidMinGas();
+
+/*
+ * a generic LzReceiver implementation
+ */
+abstract contract LzApp is Ownable, ILayerZeroReceiver, ILayerZeroUserApplicationConfig {
+    using BytesLib for bytes;
+
+    ILayerZeroEndpoint public immutable lzEndpoint;
+    mapping(uint16 => bytes) public trustedRemoteLookup;
+    mapping(uint16 => mapping(uint16 => uint256)) public minDstGasLookup;
+    address public precrime;
+
+    event SetPrecrime(address precrime);
+    event SetTrustedRemote(uint16 _remoteChainId, bytes _path);
+    event SetTrustedRemoteAddress(uint16 _remoteChainId, address _remoteAddress);
+    event SetMinDstGas(uint16 _dstChainId, uint16 _type, uint256 _minDstGas);
+
+    constructor(address _endpoint) {
+        lzEndpoint = ILayerZeroEndpoint(_endpoint);
+    }
+
+    function lzReceive(
+        uint16 _srcChainId,
+        bytes calldata _srcAddress,
+        uint64 _nonce,
+        bytes calldata _payload
+    ) public virtual override {
+        // lzReceive must be called by the endpoint for security
+        if (_msgSender() != address(lzEndpoint)) revert Forbidden();
+
+        bytes memory trustedRemote = trustedRemoteLookup[_srcChainId];
+        // if will still block the message pathway from (srcChainId, srcAddress). should not receive message from untrusted remote.
+        if (
+            !(_srcAddress.length == trustedRemote.length &&
+                trustedRemote.length > 0 &&
+                keccak256(_srcAddress) == keccak256(trustedRemote))
+        ) revert NoTrustedRemote();
+
+        _blockingLzReceive(_srcChainId, _srcAddress, _nonce, _payload);
+    }
+
+    // abstract function - the default behaviour of LayerZero is blocking. See: NonblockingLzApp if you dont need to enforce ordered messaging
+    function _blockingLzReceive(
+        uint16 _srcChainId,
+        bytes calldata _srcAddress,
+        uint64 _nonce,
+        bytes calldata _payload
+    ) internal virtual;
+
+    function _lzSend(
+        uint16 _dstChainId,
+        address _dstAddress,
+        bytes memory _payload,
+        address payable _refundAddress,
+        address _zroPaymentAddress,
+        bytes memory _adapterParams,
+        uint256 _nativeFee
+    ) internal virtual {
+        bytes memory trustedRemote = trustedRemoteLookup[_dstChainId];
+        if (trustedRemote.length == 0) revert NoTrustedRemote();
+
+        lzEndpoint.send{value: _nativeFee}(
+            _dstChainId,
+            abi.encodePacked(_dstAddress, address(this)),
+            _payload,
+            _refundAddress,
+            _zroPaymentAddress,
+            _adapterParams
+        );
+    }
+
+    function _checkGasLimit(
+        uint16 _dstChainId,
+        uint16 _type,
+        bytes memory _adapterParams,
+        uint256 _extraGas
+    ) internal view virtual {
+        uint256 providedGasLimit = _getGasLimit(_adapterParams);
+        uint256 minGasLimit = minDstGasLookup[_dstChainId][_type] + _extraGas;
+        if (minGasLimit == 0) revert MinGasLimitNotSet();
+        if (providedGasLimit < minGasLimit) revert GasLimitTooLow();
+    }
+
+    function _getGasLimit(bytes memory _adapterParams) internal pure virtual returns (uint256 gasLimit) {
+        if (_adapterParams.length < 34) revert InvalidAdapterParams();
+        assembly {
+            gasLimit := mload(add(_adapterParams, 34))
+        }
+    }
+
+    //---------------------------UserApplication config----------------------------------------
+    function getConfig(
+        uint16 _version,
+        uint16 _chainId,
+        address,
+        uint256 _configType
+    ) external view returns (bytes memory) {
+        return lzEndpoint.getConfig(_version, _chainId, address(this), _configType);
+    }
+
+    // generic config for LayerZero user Application
+    function setConfig(
+        uint16 _version,
+        uint16 _chainId,
+        uint256 _configType,
+        bytes calldata _config
+    ) external override onlyOwner {
+        lzEndpoint.setConfig(_version, _chainId, _configType, _config);
+    }
+
+    function setSendVersion(uint16 _version) external override onlyOwner {
+        lzEndpoint.setSendVersion(_version);
+    }
+
+    function setReceiveVersion(uint16 _version) external override onlyOwner {
+        lzEndpoint.setReceiveVersion(_version);
+    }
+
+    function forceResumeReceive(uint16 _srcChainId, bytes calldata _srcAddress) external override onlyOwner {
+        lzEndpoint.forceResumeReceive(_srcChainId, _srcAddress);
+    }
+
+    // _path = abi.encodePacked(remoteAddress, localAddress)
+    // this function set the trusted path for the cross-chain communication
+    function setTrustedRemote(uint16 _srcChainId, bytes calldata _path) external onlyOwner {
+        trustedRemoteLookup[_srcChainId] = _path;
+        emit SetTrustedRemote(_srcChainId, _path);
+    }
+
+    function setTrustedRemoteAddress(uint16 _remoteChainId, address _remoteAddress) external onlyOwner {
+        trustedRemoteLookup[_remoteChainId] = abi.encodePacked(_remoteAddress, address(this));
+        emit SetTrustedRemoteAddress(_remoteChainId, _remoteAddress);
+    }
+
+    function setPrecrime(address _precrime) external onlyOwner {
+        precrime = _precrime;
+        emit SetPrecrime(_precrime);
+    }
+
+    function setMinDstGas(
+        uint16 _dstChainId,
+        uint16 _packetType,
+        uint256 _minGas
+    ) external onlyOwner {
+        if (_minGas == 0) revert InvalidMinGas();
+        minDstGasLookup[_dstChainId][_packetType] = _minGas;
+        emit SetMinDstGas(_dstChainId, _packetType, _minGas);
+    }
+}
